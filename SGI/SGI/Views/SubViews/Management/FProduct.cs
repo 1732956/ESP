3e61878a07efc6a1d38b6c861b332d2831618f77
tristfect998 <tristan.tf@hotmail.com>
2020-05-14 11:47:08 -0400

    ajout installer, setup de l'istaller, modifs fichier server et client implantation, réglé bug dans fenêtre produit avec preview étiquette

--- SGI/SGI/Views/SubViews/Management/FProduct.cs
+++ SGI/SGI/Views/SubViews/Management/FProduct.cs
@@ -460,14 +460,14 @@ namespace SGI.Views.SubViews
                 using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
                     Font oFont = new System.Drawing.Font("IDAHC39M Code 39 Barcode", 20);
-                    Font oFontSec = new System.Drawing.Font("Arial", 20);
+                    Font oFontSec = new System.Drawing.Font("Arial", 18);
                     PointF point = new PointF(2f, 50f);
                     PointF Secpoint = new PointF(20f, 2f);
                     SolidBrush black = new SolidBrush(Color.Black);
                     SolidBrush white = new SolidBrush(Color.White);
                     graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
                     graphics.DrawString("*" + barcode + "*", oFont, black, point);
-                    graphics.DrawString("test nom produit", oFontSec, black, Secpoint);
+                    graphics.DrawString(currentProduct.Name, oFontSec, black, Secpoint);
 
                 }
                 pictureBox2.Image = bitmap;
